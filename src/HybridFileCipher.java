5ac1b5bb5e66c2f8f5940427e86e55807ff0c86a
afonso-cabaco <a47179@alunos.isel.pt>
2022-10-20 21:09:06 +0100

    Progress
    getPrivatekey method
    pickOptionMessage method
    refactor main method

--- src/HybridFileCipher.java
+++ src/HybridFileCipher.java
@@ -16,11 +16,13 @@ Considere os ficheiros .cer e .pfx em anexo ao enunciado onde estão chaves púb
 para testar a aplicação.
  */
 
+import org.apache.commons.codec.binary.Base64InputStream;
+import org.apache.commons.codec.binary.Base64OutputStream;
 
 import javax.crypto.*;
 import java.io.File;
 import java.io.FileInputStream;
-
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -28,39 +30,54 @@ import java.security.*;
 import java.security.cert.Certificate;
 import java.security.cert.CertificateException;
 import java.security.cert.CertificateFactory;
+import java.util.Enumeration;
+import java.util.Scanner;
 
 
 public class HybridFileCipher {
-    /*
-    arg[0] - modo
-    arg[1] - ficheiro
-    arg[2] - cert
-     */
-    public static void main(String[] args)
-            throws
-            NoSuchAlgorithmException,
-            NoSuchPaddingException,
-            InvalidKeyException,
-            IOException,
-            IllegalBlockSizeException,
-            BadPaddingException, KeyStoreException, CertificateException, UnrecoverableKeyException {
-        File file = new File(args[1]);
-        if(args[0].compareTo("enc") == 0){
-            File [] files = HybridCipher(file, args[2]);
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+        String[] option;
+
+        pickOptionMessage();
+
+        while (true){
+
+            try{
+
+                option = scanner.nextLine().split(" ");
+                switch (option[0]) {
+                    case "-exit":
+                        System.out.println("Closing ...");
+                        System.exit(1);
+                    case "-enc":
+                        HybridCipher(option[1], option[2]);
+                        break;
+                    case "-dec":
+                        HybridDecipher(option[1], option[2], getPrivateKey(option[3]));
+                    default:
+                        pickOptionMessage();
+                        break;
+                }
+            } catch (Exception ignored) {
+                System.out.println("Something went wrong!");
+            }
         }
-        else if(args[0].compareTo("dec") == 0){
-            File f = HybridDecipher(args[1], args[2], args[3], args[4]);
-        }
+
     }
 
-    private static File[] HybridCipher(File fileToCipher, String certificateName) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException, IOException, CertificateException {
+    private static void HybridCipher(String fileToCipher, String certificateName) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException, IOException, CertificateException {
+
         KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
         SecretKey secretKey = keyGenerator.generateKey();
 
         Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
         cipher.init(Cipher.ENCRYPT_MODE, secretKey);
 
-        byte [] cipheredFileBytes = cipher.doFinal(Files.readAllBytes(fileToCipher.toPath()));
+        File file = new File(fileToCipher);
+
+        byte [] cipheredFileBytes = cipher.doFinal(Files.readAllBytes(file.toPath()));
         File cipheredFile = new File("src/CipheredFile.txt");
         Files.write(Path.of(cipheredFile.getPath()), cipheredFileBytes);
 
@@ -78,40 +95,81 @@ public class HybridFileCipher {
         byte [] cipheredKeyBytes = cipherAsymmetric.wrap(secretKey);
         File cipheredKey = new File("src/CipheredKeyBytes.txt");
         Files.write(Path.of(cipheredKey.getPath()), cipheredKeyBytes);
-
-        return new File[]{};
     }
 
-    private static File HybridDecipher(String fileName, String keyFileName, String pfxFileName, String password) throws KeyStoreException, IOException, CertificateException, NoSuchAlgorithmException, InvalidKeyException, NoSuchPaddingException, UnrecoverableKeyException, IllegalBlockSizeException, BadPaddingException {
-        KeyStore ks = KeyStore.getInstance("PKCS12");
+    private static void HybridDecipher(String cipheredText, String keyFile, Key privateKey) throws Exception {
 
-        char [] passwordArray = password.toCharArray();
+
+        //Cipher Setup for unwrap using the given Private Key
+        Cipher decipherKey = Cipher.getInstance("RSA");
+        decipherKey.init(Cipher.UNWRAP_MODE, privateKey);
+
+        //Ciphered Secret Key
+        FileInputStream keyFis = new FileInputStream(keyFile);
+        Base64InputStream keyBIS = new Base64InputStream(keyFis);
+
+        //Unwrapping the secret Key
+        byte [] wrappedKey = keyBIS.readAllBytes();
+        SecretKey secretKey = (SecretKey) decipherKey.unwrap(wrappedKey, "AES", Cipher.SECRET_KEY);
+
+        //New decipher to be used to obtain the ciphered message
+        Cipher decipherMsg = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        decipherMsg.init(Cipher.DECRYPT_MODE, secretKey);
+
+        //Ciphered Message
+        FileInputStream textFIS = new FileInputStream(cipheredText);
+        Base64InputStream textBIS = new Base64InputStream(textFIS);
+
+        //Output for the message after deciphered
+        FileOutputStream decipheredFIS = new FileOutputStream("src/DecipheredText.txt");
+        Base64OutputStream decipheredBIS = new Base64OutputStream(decipheredFIS);
+
+        //Reading and deciphering the message
+        int read;
+        byte[] buffer = new byte[1000];
+        while((read = textBIS.read(buffer)) != -1){
+            byte[] data = decipherMsg.update(buffer, 0, read);
+            decipheredBIS.write(data);
+        }
+        decipheredBIS.write(decipherMsg.doFinal());
+        decipheredBIS.flush();
+
+        textBIS.close();
+        textFIS.close();
+        decipheredBIS.close();
+        decipheredFIS.close();
+        keyBIS.close();
+        keyFis.close();
+
+    }
+
+    private static Key getPrivateKey(String pfxFile) throws Exception{
+
+        KeyStore ks = KeyStore.getInstance("PKCS12");
+        char [] passwordArray = "changeit".toCharArray();
 
         ks.load(
-                new FileInputStream(pfxFileName),
+                new FileInputStream(pfxFile),
                 passwordArray
         );
-        String alias = ks.aliases().nextElement();
-        PrivateKey privateKey = (PrivateKey) ks.getKey(alias, passwordArray);
 
-        //get secretKey
-        Cipher cipher = Cipher.getInstance("RSA");
-        cipher.init(Cipher.UNWRAP_MODE, privateKey);
+        Enumeration<String> entries = ks.aliases();
 
-        byte [] wrappedKey = new FileInputStream(keyFileName).readAllBytes();
-        SecretKey secretKey = (SecretKey) cipher.unwrap(wrappedKey, "RSA", Cipher.SECRET_KEY);
+        if(entries.hasMoreElements())
+            return ks.getKey(entries.nextElement(), passwordArray);
 
-        //get message
-        Cipher cipherMsg = Cipher.getInstance("AES/CBC/PKCS5Padding");
-        cipher.init(Cipher.DECRYPT_MODE, secretKey);
-
-        byte [] msgBytes = new FileInputStream(fileName).readAllBytes();
-        msgBytes = cipherMsg.doFinal(msgBytes);
-
-        File file = new File("src/MessageFile.txt");
-        Files.write(Path.of(file.getPath()), msgBytes);
-
-        return file;
+        throw new Exception("No Private Key present in the given Certificate");
     }
 
+    private static void pickOptionMessage(){
+        System.out.println(
+                """
+                        Welcome!\s
+                        Select one of the following options:\s
+                        -enc {file-to-encode} {certificate-with-public-key}\s
+                        -dec {file-to-decode} {secret-key-wrapped} {pfx-with-private-key}\s
+                        -exit Exits.
+                        """
+        );
+    }
 }
