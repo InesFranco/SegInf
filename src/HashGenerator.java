9ee7af9c4c5fe09a3b8df2498b3de63727214581
afonso-cabaco <a47179@alunos.isel.pt>
2022-10-27 17:53:52 +0100

    HashGenerator - ex5

--- src/HashGenerator.java
+++ src/HashGenerator.java
@@ -1,37 +1,55 @@
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.File;
+
+import java.nio.file.Files;
 import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
+import java.util.Scanner;
 
+/**
+ * Um dos Testes Realizados
+ *
+ * SHA1 CA1_int.cer
+ * output -> 1b8d5ccf68b3bc642ae22bef68b94284acab66c3
+ **/
 
+public class HashGenerator {
 
-public class HashGenerator{
-    public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
-        FileInputStream in = null;
-        MessageDigest md = MessageDigest.getInstance("SHA-1");
+    public static void main(String[] args) throws Exception {
 
-        try {
-            in = new FileInputStream("C:/Users/Ines/OneDrive - Instituto Superior de Engenharia de Lisboa/Desktop/Universidade/SegInf/SegInf/Enunciado/certificates-and-keys/cert-int/CA1-int.cer");
-            byte msg;
-            while ((msg = (byte) in.read()) != -1) {
-                md.update(msg);
-            }
-            byte[] h = md.digest();
-            prettyPrint(h);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } finally {
-            if (in != null) {
-                in.close();
-            }
+        Scanner scanner = new Scanner(System.in);
+        String[] input = null;
+
+        while(input == null) {
+            System.out.println(
+                    """
+                            Welcome!\s
+                            Insert a hash function and the file you want to obtain the hash from:
+                            """
+            );
+
+            input = scanner.nextLine().split(" ");
         }
+
+        String hashName = input[0];
+        StringBuilder filePath = new StringBuilder(input[1]);
+
+        for (int i = 2; i < args.length; i++)
+            filePath.append(" ").append(args[i]);
+
+
+        File file = new File(filePath.toString());
+        byte[] fileToByte = Files.readAllBytes(file.toPath());
+
+        MessageDigest md = MessageDigest.getInstance(hashName);
+        md.update(fileToByte);
+        byte[] h = md.digest();
+        prettyPrint(h);
     }
 
-    private static void prettyPrint(byte[] h) {
-        for (byte b : h) {
+    private static void prettyPrint(byte[] tag) {
+        for (byte b : tag) {
             System.out.printf("%02x", b);
         }
         System.out.println();
+
     }
 }
