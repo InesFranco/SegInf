220eb583523ef348f9cec8b7114f2251ca68df53
Afonso Cabaço <afonso.cabaco@icloud.com>
2022-12-11 18:48:57 +0000

    tp2-ex6 start config +-

---
+++ tp2/https-server/https-server-base.js
@@ -0,0 +1,36 @@
+// Built-in HTTPS support
+const https = require("https");
+// Handling GET request (npm install express)
+const express = require("express");
+// Load of files from the local file system
+const fs = require('fs');
+
+const PORT = 4433;
+const app = express();
+
+// Get request for resource /
+app.get("/", function (req, res) {
+    console.log(
+        req.socket.remoteAddress
+        //+ ' ' + req.socket.getPeerCertificate().subject.CN
+        + ' ' + req.method
+        + ' ' + req.url);
+    res.send("<html><body>Secure Hello World with node.js</body></html>");
+});
+
+
+// configure TLS handshake
+const options = {
+    key: fs.readFileSync('secure-server-key.pem'),
+    cert: fs.readFileSync('secure-server-cert.pem'),
+    ca: fs.readFileSync('<server trustbase PEM (root CA)>'),
+    requestCert: true,
+    rejectUnauthorized: true
+};
+
+// Create HTTPS server
+https.createServer(options, app).listen(PORT, 
+    function (req, res) {
+        console.log("Server started at port " + PORT);
+    }
+);
